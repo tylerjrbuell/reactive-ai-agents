b956bfb9c2db6760be8efbe0fd0d53a6b06fe1b5
Tyler Buell <tylerjrbuell@gmail.com>
2025-05-06 12:05:26 -0400

    Revert "chore: bump version"
    This reverts commit a9d9ff5b0c9d51ec58acec6e7f8f9b79213a4c04.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="reactive-agents",
-    version="0.1.0",
+    version="0.2.0",
     author="Tyler Buell",
     description="A custom reactive AI Agent framework for LLM-driven task execution",
     long_description=long_description,
